45e0c74d26a739390cb91e924ef0101e2650a9ae
Thomas Sievert <tcsievert@gmail.com>
2020-06-20 19:55:28 -0600

    updated makefile to properly run arraylist

--- arraylist/main_arraylist.c
+++ arraylist/main_arraylist.c
@@ -14,11 +14,18 @@ typedef struct
 struct main(int argc, int *argv[])
 {
     /* function calls */
+    arraylist *my_arraylist = malloc(16);
+    
 }
 
 /* allocates memory to the base array based upon a user-input initial capacity */
 int initialize_arraylist(struct *arraylist_reference, int initial_capacity)
 {
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
     *arraylist_reference.zeroth_index = malloc(4 * initial_capacity);
     /* sets the capacity to the initial user-input capacity */
     *arraylist_reference.capacity = initial_capacity;
@@ -28,6 +35,11 @@ int initialize_arraylist(struct *arraylist_reference, int initial_capacity)
 /* reallocates memory to a new base array with double capacity of the original one */
 int resize_arraylist(struct *arraylist_reference)
 {
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
     *arraylist_reference.zeroth_index = realloc(4 * (2 * *arraylist_reference.capacity); 
     /* sets the capacity to double the previous capacity */
     *arraylist_reference.capacity = 2 * initial_capacity;
@@ -38,6 +50,11 @@ int resize_arraylist(struct *arraylist_reference)
 /* reallocates memory to a new base array with a user-input new capacity */
 int resize_arraylist(struct *arraylist_reference, int new_capacity)
 {
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
     *arraylist_reference.zeroth_index = realloc(4 * new_capacity);
     /* sets the capacity to the new user-input capacity */
     *arraylist_reference.capacity = new_capacity;
@@ -47,6 +64,11 @@ int resize_arraylist(struct *arraylist_reference, int new_capacity)
 /* frees memory allocation for base array */
 int free_arraylist(struct *arraylist_reference)
 {
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
     free(*arraylist_reference.zeroth_index);
     return 0;
 }     
@@ -54,6 +76,11 @@ int free_arraylist(struct *arraylist_reference)
 /* appends a user-input value to the end of the current list (by size, not capacity) */
 int add(struct *arraylist_reference, int value)
 {
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
     /* checks if resizing is needed */
     if(*arraylist_reference.size == *arraylist_reference.capacity)
     {
@@ -69,6 +96,11 @@ int add(struct *arraylist_reference, int value)
 /* appends a user-input value to the end of the current list (by size, not capacity) */
 int add(struct *arraylist_reference, int value, int index)
 {
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
     /* checks if user-input index is valid */
     if!(0 <= index && index <= size)
     {
@@ -89,4 +121,67 @@ int add(struct *arraylist_reference, int value, int index)
     /* incriments size variable in arraylist struct */
     *arraylist_reference.size++;
     return 0;
+}
+
+int remove(struct *arraylist_reference, int index)
+{
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
+    /* checks if user-input index is valid */
+    if!(0 <= index && index <= size)
+    {
+      return 1;
+    }
+    /* checks if index is the end of the array (by size) */
+    if(size - index == 0)
+    {
+        *arraylist_reference.size--;
+        return 0;
+    }
+    /* loops through array, shifting values downward*/
+    else
+    {
+        for(int i = 0; i < (size - index) - 1; i++)
+        {
+            arraylist_reference[index + i] = arraylist_reference[(index + 1) + i];
+        }
+        return 0;
+    }
+}
+
+/* looks for first occurrence of user-input value and removes that value if one is found */
+int remove(struct *arraylist_reference, int value)
+{
+    /* checks if pointer passed was valid */
+    if(arraylist_reference == NULL)
+    {
+        return 1;
+    }
+    /* loops through base array to find first occurrence of user-input value */
+    for(int *i = arraylist_reference; i < arraylist_reference + (4 * size); i += 4)
+    {
+        /* checks if current value matches user-input value */
+        if(*i == value)
+        {
+            *arraylist_reference.size--;
+            int shift_begin = 1;
+        }
+        /* performs the backward shift after user-input value is found */
+        if(shift_begin == 1)
+        {
+            *i = *(i + 4);
+        }
+    }
+    /* checks if value was found in base array */
+    if(shift_begin == 1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
